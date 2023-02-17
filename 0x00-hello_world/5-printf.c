5feb0b4ccb3e215509a0f9fe9148b9241acd731a
ibtihajemil <ibtihajemil@gmail.com>
2023-02-16 17:47:09 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main - script printing using print function
-* Return: Always 0 (success)
-*/
+ * main - script printing using print function
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-printf("with propper grammar, but the outcome is a piece of art, \n");
+printf("with propper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
 
